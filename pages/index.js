16470c614b56f7b9c8463c9dd5e21ad4c09321cf
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-06-05 16:46:25 +0600

    api

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,26 @@
 import Link from 'next/link'
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from "@material-tailwind/react";
 import Header from './layouts/Header';
+import { useRouter } from 'next/router';
 
 export default function home() {
+
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const router = useRouter();
+
+  const handleLogin = () => {
+    router.push('/login');
+  }
+
   return (
     <>
       <Header />
       <h1>This is home page</h1>
+
+      {
+        isLoggedIn ? <h1>Logged In</h1> : <Button color="blue" onClick={handleLogin}>Login</Button>
+      }
 
     </>
   )
