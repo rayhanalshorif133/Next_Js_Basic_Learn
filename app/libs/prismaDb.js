35088e30c312d8c64c092ac961f000e3d70ce04a
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-07-13 17:38:33 +0600

    working on it

---
+++ app/libs/prismaDb.js
@@ -0,0 +1,9 @@
+import { PrismaClient } from '@prisma/client'
+
+const client = new PrismaClient();
+
+if(process.env.NODE_ENV !== 'production') {
+  globalThis.prisma = client;
+}
+
+export default client;
