bafa062a6a57aa8fefe2e1241c00a090ddbecdba
rayhanalshorif133 <rayhanalshorif.edu2@gmail.com>
2023-07-16 09:53:20 +0600

    delete prism

--- app/libs/prismaDb.js
+++
@@ -1,9 +0,0 @@
-import { PrismaClient } from '@prisma/client'
-
-const client = new PrismaClient();
-
-if(process.env.NODE_ENV !== 'production') {
-  globalThis.prisma = client;
-}
-
-export default client;
